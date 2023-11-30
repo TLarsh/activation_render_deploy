eb49a5170c5e00a3798a1affc79c2f8e18bf5bb0
ADEYEMI AYOBAMI SAMUEL <107284584+TLarsh@users.noreply.github.com>
2023-11-30 14:46:42 +0100

    Update settings.py

--- activation/settings.py
+++ activation/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 # STATIC_ROOT = BASE_DIR / 'static/'
 # MEDIA_ROOT = BASE_DIR / 'media/'
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
